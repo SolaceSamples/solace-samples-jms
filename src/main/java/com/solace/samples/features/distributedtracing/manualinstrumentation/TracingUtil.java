665dedde1f9f3d4613e7796cef7097629bfed8ed
Jamil Ahmed <35374694+itsJamilAhmed@users.noreply.github.com>
2024-05-20 16:58:19 +0100

    Update TracingUtil.java

--- src/main/java/com/solace/samples/features/distributedtracing/manualinstrumentation/TracingUtil.java
+++ src/main/java/com/solace/samples/features/distributedtracing/manualinstrumentation/TracingUtil.java
@@ -22,6 +22,7 @@ package com.solace.samples.features.distributedtracing.manualinstrumentation;
 import com.solace.opentelemetry.javaagent.jms.SolaceJmsContextPropagator;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
@@ -30,33 +31,43 @@ import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import java.util.concurrent.TimeUnit;
 
+//A class to facilitate OpenTelemetry Instrumentation. Can be commonly used across the application.
 public class TracingUtil {
 
-  private TracingUtil() {
-  }
+	private TracingUtil() {}
 
-  public static void initManualTracing(String serviceName) {
-    //Create Resource
-    Resource resource = Resource.getDefault().merge(Resource.create(
-        Attributes.of(ResourceAttributes.SERVICE_NAME, serviceName)));
+	public static void initManualTracing(String serviceName) {
 
-    //Create Span Exporter
-    OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
-        //.setEndpoint("http://localhost:55680")
-        .build();
+		// OpenTelemetry Resource object
+		Resource resource = Resource.getDefault().merge(Resource.create(
+				Attributes.of(ResourceAttributes.SERVICE_NAME, serviceName)));
 
-    //Create SdkTracerProvider
-    SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-        .addSpanProcessor(BatchSpanProcessor.builder(spanExporter)
-            .setScheduleDelay(100, TimeUnit.MILLISECONDS).build())
-        .setResource(resource)
-        .build();
+		// OpenTelemetry provides gRPC, HTTP and NoOp span exporter.
+		// Configure the endpoint details dependent on the protocol choice for your OTLP endpoint
 
-    //This Instance can be used to get tracer if it is not configured as global
-    OpenTelemetrySdk.builder()
-        .setTracerProvider(sdkTracerProvider)
-        //.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-        .setPropagators(ContextPropagators.create(new SolaceJmsContextPropagator()))
-        .buildAndRegisterGlobal();
-  }
+		// If gRPC:
+		OtlpGrpcSpanExporter spanExporterGrpc = OtlpGrpcSpanExporter.builder()
+			.setEndpoint("http://localhost:4317")
+			.build();
+
+		// If HTTP:
+		OtlpHttpSpanExporter spanExporterHttp = OtlpHttpSpanExporter.builder()
+			.setEndpoint("https://yourhost.com/opentelemetry/public/v1/traces/")
+			.addHeader("authorization", "dataKey example-key")
+			.build();
+
+		// Use OpenTelemetry SdkTracerProvider as TracerProvider
+		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+			.addSpanProcessor(BatchSpanProcessor.builder(spanExporterGrpc)
+					.setScheduleDelay(1000, TimeUnit.MILLISECONDS).build())
+			.setResource(resource)
+			.build();
+
+		// This Instance can be used to get tracer if it is not configured as global
+		OpenTelemetrySdk.builder()
+			.setTracerProvider(sdkTracerProvider)
+			//.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+			.setPropagators(ContextPropagators.create(new SolaceJmsContextPropagator()))
+			.buildAndRegisterGlobal();
+	}
 }
